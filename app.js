9583d561bb5b3cbecaf9757c4af000938ec2b2fa
Calvin T <58273749+Calvin087@users.noreply.github.com>
2022-02-19 13:12:14 +0100

    adding app base

---
+++ app.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+
+const createError = require("http-errors");
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const passport = require("passport");
+const sessionConfig = require("./config/session.config");
+
+require("./config/db.config");
+require("./config/hsb.config");
+require("./config/passport.config");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));
+
+app.use(sessionConfig);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// TODO
+// need locals.currentUser for state
+// app.use(() => {
+//   res.locals.currentUser = req.user;
+//   next();
+// });
+
+const router = require("./config/routes.config");
+app.use("/", router);
+
+// ERROR HANDLING
+
+app.use((req, res, next) => {
+  next(createError(404, "Page not found"));
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  let status = error.status || 500;
+
+  res.status(status).render("error", {
+    message: error.message,
+    error: req.app.get("env") === "development" ? error : {},
+  });
+});
+
+// TODO
+// Does this need to be different to 3000 when live?
+const port = Number(process.env.PORT || 3000);
+
+app.listen(port, () => {
+  console.log(`App listen on port ${port}`);
+});
