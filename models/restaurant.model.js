8c0006bf4a658cca6c92fd44ec6c3c03c546f39c
Calvin T <58273749+Calvin087@users.noreply.github.com>
2022-02-21 21:25:08 +0100

    cleaning seed data

--- models/restaurant.model.js
+++ models/restaurant.model.js
@@ -1,48 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const restaurantSchema = new Schema({
+const restaurantSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Please enter name' ]
+      type: String,
+      required: [true, "Please enter name"],
     },
     address: {
-        type: String,
-        required: [true, 'Please enter address']
+      type: String,
+      required: [true, "Please enter address"],
     },
     phone: {
-        type: String,
-        required: [true, 'Please enter a valid telephone number']
+      type: String,
+      required: [true, "Please enter a valid telephone number"],
     },
     website: {
-        type: String
+      type: String,
     },
     schedule: {
-        type: String,
-        default: 'Please check opening hours on the website'
+      type: String,
+      default: "Please check opening hours on the website",
     },
+    dishType: {
+      type: String,
+    },
+    veganOptions: {
+      type: String,
+    }
     description: {
-        type: String,
-        required: [true, 'Please, tell us something about this place!']
+      type: String,
+      required: [true, "Please, tell us something about this place!"],
     },
     fullVegan: {
-        type: Boolean
+      type: Boolean,
     },
     takeOut: {
-        type: Boolean
+      type: Boolean,
     },
     image: {
-        type: String,
-        default: 'https://images.unsplash.com/photo-1560155016-bd4879ae8f21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
-    }
-
-}, { timestamps: true }
+      type: [String],
+      default:
+        "https://images.unsplash.com/photo-1560155016-bd4879ae8f21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    },
+  },
+  { timestamps: true }
 );
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 module.exports = Restaurant;
-
-
-
-
-
