98194352a67b639d5a7801715a6cd982a407039b
Maria Gallego <mariagestionredes@gmail.com>
2022-02-24 12:42:20 +0100

    created categories for new rest form

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -1,30 +1,53 @@
 const mongoose = require("mongoose");
-const Restaurant = require('../models/restaurant.model');
-
+const Restaurant = require("../models/restaurant.model");
+const categories = Object.keys(require("../data/categories.json"));
 
 module.exports.list = (req, res, next) => {
   Restaurant.find()
-  .limit(12)
-  .then((restaurants) => res.render('restaurants/list', { restaurants }))
-  .catch((error) => next(error));
+    .limit(12)
+    .then((restaurants) => res.render("restaurants/list", { restaurants }))
+    .catch((error) => next(error));
 };
 
 module.exports.detail = (req, res, next) => {
   Restaurant.findById(req.params.id)
-  .then((restaurant) => {
-    if (restaurant) {
-      res.render('restaurants/detail', { restaurant });
-    } else {
-      res.redirect('/restaurants');
-    }
-  })
-  .catch((error) => next(error));
+    .then((restaurant) => {
+      if (restaurant) {
+        res.render("restaurants/detail", { restaurant });
+      } else {
+        res.redirect("/restaurants");
+      }
+    })
+    .catch((error) => next(error));
 };
 
 module.exports.create = (req, res, next) => {
-  res.render('restaurants/new')
+  res.render('restaurants/new', {
+    categories: categories
+  });
 };
 
 module.exports.doCreate = (req, res, next) => {
-/* aquí la información del form la recibimos nosotros para crear el restaurant */
-}
+  /* aquí la información del form la recibimos nosotros para crear el restaurant */
+  const restaurantCategories = req.body.categories;
+  
+  /* este método comprueba si en el body llega String o Array y si es String, lo convierte en Array, que es lo que se espera */
+  if (restaurantCategories && !Array.isArray(restaurantCategories)) {
+    restaurantCategories = [restaurantCategories]
+  }
+
+  const restaurant = new Restaurant({
+    name: req.body.name,
+    address: req.body.address,
+    phone: req.body.phone,
+    website: req.body.website,
+    schedule: req.body.schedule,
+    dishType: req.body.dishType,
+    veganOptions: req.body.veganOptions,
+    description: req.body.description,
+    fullVegan: req.body.fullVegan,
+    takeOut: req.body.takeOut,
+    categories: restaurantCategories,
+    image: req.body.image || undefined
+  });
+};
