7b87c3d6e9818e027cc90eae019acb97a502bc7a
Calvin T <58273749+Calvin087@users.noreply.github.com>
2022-02-26 18:24:27 +0100

    cloudinary images working, new comments working, add new comment working

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-// MODELS GO HERE
+const upload = require("../config/storage.config");
+const Comment = require("../models/comments.model");
 
 // MODELS GO HERE
 // PASSPORT
@@ -10,7 +11,21 @@ module.exports.create = (req, res, next) => {
 };
 
 module.exports.doCreate = (req, res, next) => {
-  console.log(req.body);
+  const { restaurant, user, rating, description } = req.body;
+  const fileName = req.file.path;
+
+  Comment.create({
+    restaurant,
+    user,
+    rating,
+    description,
+    images: fileName,
+  })
+    .then((newComment) => {
+      console.log(newComment);
+      res.redirect(`/restaurants/${restaurant}`);
+    })
+    .catch((error) => console.log(error));
 };
 
 module.exports.edit = (req, res, next) => {
