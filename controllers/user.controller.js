8953d332dabcfd32aab4f599f82b283ae9d22120
Calvin T <58273749+Calvin087@users.noreply.github.com>
2022-02-28 22:13:07 +0100

    showing all comments and all fav avocados on profile page

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,25 @@
+const { localsAsTemplateData } = require("hbs");
 const mongoose = require("mongoose");
+const Comment = require("../models/comments.model");
+const Avocado = require("../models/avocado.model");
+const Restaurant = require("../models/restaurant.model");
 // MODELS GO HERE
 
 // MODELS GO HERE
 // PASSPORT
 // MAILER...?
 
-module.exports.profile = (req, res, next) => {
-  res.render("user/profile");
+module.exports.profile = async (req, res, next) => {
+  const userDetails = res.locals.currentUser;
+
+  const comments = await Comment.find({ user: userDetails._id });
+  const avocados = await Avocado.find({ user: userDetails._id });
+  const favouriteLocations = avocados.map((avocado) => {
+    return avocado.restaurant;
+  });
+  Restaurant.find({ _id: favouriteLocations }).then((favourites) => {
+    res.render("user/profile", { comments, favourites });
+  });
 };
 
 module.exports.edit = (req, res, next) => {
