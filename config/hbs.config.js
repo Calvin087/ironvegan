962091348361a9e3cc41046032b20ee5326f0225
Robert Cabrera <90862167+robertcach@users.noreply.github.com>
2022-03-03 13:07:16 +0100

    hbs.config is ok now.

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -6,6 +6,8 @@ const { mongoose } = require("mongoose");
 // partials
 hbs.registerPartials("./views/partials"); // to do
 
+// helpers down here
+
 hbs.registerHelper("userDoAvocado", function (options) {
   const { restaurant, avocados } = options.hash;
   /* .some checks if at least one element pass the condition --> true */
@@ -35,15 +37,6 @@ hbs.registerHelper("isUserReview", function (options) {
   const { comment, user } = options.hash;
   console.log(user)
   if (comment.user._id.equals(user._id)) {
-
-hbs.registerHelper("userDoAvocado", function (options) {
-  const { restaurant, avocados } = options.hash;
-  /* .some checks if at least one element pass the condition --> true */
-  if (
-    restaurant &&
-    avocados &&
-    avocados.some((avocado) => avocado.restaurant == restaurant.id)
-
     return options.fn(this);
   } else {
     return options.inverse(this);
