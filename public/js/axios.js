ceb3dd2c8a2a73ca5e04cbca0121ebb65767eebb
Calvin T <58273749+Calvin087@users.noreply.github.com>
2022-03-11 23:35:35 +0100

    axios not working online

--- public/js/axios.js
+++ public/js/axios.js
@@ -1,17 +1,17 @@
 const httpClient = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: "http://localhost:3000",
 });
 
 document.querySelectorAll(".list-page-advocado").forEach((btn) => {
-    btn.onclick = (event) => {
-      avocadoRestaurant(event.target.dataset.id, event.target); // dataset para acceder al atributo del 🥑 --> contiene la id del restaurante
-    };
-  });
-
-  const avocadoRestaurant = (id, image) => {
-      image.classList.toggle('list-page-advocado-active');
-      httpClient.post(`/avocado/${id}`).catch((error) => next(error));
+  btn.onclick = (event) => {
+    avocadoRestaurant(event.target.dataset.id, event.target); // dataset para acceder al atributo del 🥑 --> contiene la id del restaurante
   };
+});
+
+const avocadoRestaurant = (id, image) => {
+  image.classList.toggle("list-page-advocado-active");
+  httpClient.post(`/avocado/${id}`).catch((error) => console.log(error));
+};
 
 /* 
   const deleteComment = (id) => httpClient.delete(`/comment/${id}`)
